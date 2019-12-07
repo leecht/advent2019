a5a3487cf7b0cbf54ab90fe8bb7e2e40e6adbb11
leecht <58469760+leecht@users.noreply.github.com>
2019-12-07 17:55:46 +0000

    Create Ywire.java

---
+++ day3/Ywire.java
@@ -0,0 +1,13 @@
+public class Ywire {
+    public int x = 0;
+    public int nYstart = 0;
+    public int nYfinish = 0;
+
+    public Ywire (int x, int nYstart, int nYfinish) {
+        this.x = x;
+        this.nYstart = nYstart;
+        this.nYfinish = nYfinish;
+//        System.out.println("Ywire: [" + x + "][" + nYstart + "] to [" + x + "][" + nYfinish + "]");
+    }
+    
+}
