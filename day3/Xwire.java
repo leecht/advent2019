129daaccaac76a31a496c27442ae35b3885c7fb6
leecht <58469760+leecht@users.noreply.github.com>
2019-12-07 17:55:17 +0000

    Create Xwire.java

---
+++ day3/Xwire.java
@@ -0,0 +1,13 @@
+public class Xwire {
+    public int y = 0;
+    public int nXstart = 0;
+    public int nXfinish = 0;
+
+    public Xwire (int y, int nXstart, int nXfinish) {
+        this.y = y;
+        this.nXstart = nXstart;
+        this.nXfinish = nXfinish;
+//        System.out.println("Xwire: [" + nXstart + "][" + y + "] to [" + nXfinish + "][" + y + "]");
+    }
+    
+}
